d3365e07b4faf4d50ea2534ecd5ab8e9c2c32941
qingyiwebt <qingyiweb@gmail.com>
2023-09-16 21:28:17 +0800

    chore: iOS post-build script

--- scripts/ios-link.sh
+++ scripts/ios-link.sh
@@ -1,5 +1,4 @@
 #!/bin/zsh
-echo "[iOS-build] vcpkg root path: $1"
 
 VCPKG_DIR=$1
 OUTPUT_DIR=$(pwd)
@@ -8,17 +7,29 @@ PACKAGES_DIR=$VCPKG_DIR/packages
 
 TRIPLET=arm64-ios-bitcode
 
+echo "[iOS-build] ===== Unimage iOS Post-build Script ====="
+echo "[iOS-build] VCPKG_DIR: $VCPKG_DIR"
+echo "[iOS-build] OUTPUT_DIR: $OUTPUT_DIR"
+echo "[iOS-build] SCRIPT_DIR: $SCRIPT_DIR"
+echo "[iOS-build] PACKAGES_DIR: $PACKAGES_DIR"
+echo "[iOS-build] TRIPLET: $TRIPLET"
+
+echo "[iOS-build] List Of $OUTPUT_DIR"
+ls -al $OUTPUT_DIR
+
 function ExtractVcpkgArchive {
     ar x $PACKAGES_DIR/$1_$TRIPLET/lib/$2
 }
 
 function ExtractArchive {
-    echo "[iOS-build] extract library: $1"
+    echo "[iOS-build] Extract library: $1"
     ar x $1
 }
 
 mkdir _ARCHIVE
 pushd _ARCHIVE
+
+echo "[iOS-build] Extract libraries"
 
 # Extract all archives
 ExtractArchive $OUTPUT_DIR/libUnimage.a
@@ -28,7 +39,7 @@ ExtractVcpkgArchive libwebp libwebpdecoder.a
 ExtractVcpkgArchive libwebp libwebpdemux.a
 
 # Link all objects
-echo "[iOS-build] link objects"
+echo "[iOS-build] Link objects"
 ld -r *.o -o $OUTPUT_DIR/libUnimage.o -bitcode_bundle -exported_symbols_list $SCRIPT_DIR/export-symbols.txt
 rm $OUTPUT_DIR/libUnimage.a
 ar -q $OUTPUT_DIR/libUnimage.a $OUTPUT_DIR/libUnimage.o
